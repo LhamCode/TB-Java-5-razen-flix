80aa5775bc8bd22922d713486a7f70e98f2a0697
Luã Moura <130193631+LhamCode@users.noreply.github.com>
2024-05-23 08:34:37 -0300

    :sparkles: feat: adicionando primeiro GET

--- src/main/java/br/org/olabi/razenflix/RazenflixApplication.java
+++ src/main/java/br/org/olabi/razenflix/RazenflixApplication.java
@@ -2,12 +2,19 @@ package br.org.olabi.razenflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RazenflixApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RazenflixApplication.class, args);
 	}
-
+	//http://localhost:8080/hello
+	@GetMapping("/hello")
+	public String hello(){
+		return "Salve, Mundão!";
+    }
 }

516ed57aafda25747f4f9d71f4ec764efc45fc1a
Luã Moura <130193631+LhamCode@users.noreply.github.com>
2024-05-24 22:11:35 -0300

    :spearkles: feat: adicionando values e series

--- src/main/java/br/org/olabi/razenflix/RazenflixApplication.java
+++ src/main/java/br/org/olabi/razenflix/RazenflixApplication.java
@@ -1,13 +1,30 @@
 package br.org.olabi.razenflix;
 
+import br.org.olabi.razenflix.model.entity.Filme;
+import br.org.olabi.razenflix.model.value.Serie;
+import br.org.olabi.razenflix.repository.FilmeRepository;
+import br.org.olabi.razenflix.repository.SerieRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class RazenflixApplication {
+
+	private final FilmeRepository repository;
+
+	public RazenflixApplication(FilmeRepository repository){
+		this.repository = repository;
+	}
+
+	private final SerieRepository repositoryS;
+
+	public RazenflixApplication(SerieRepository repositoryS){
+		this.repositoryS = repositoryS;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RazenflixApplication.class, args);
@@ -17,4 +34,14 @@ public class RazenflixApplication {
 	public String hello(){
 		return "Salve, Mundão!";
     }
+
+	@GetMapping("/filmes")
+	public List<Filme> getFilmes(){
+		return repository.findAll();
+	}
+
+	@GetMapping("/series")
+	public List<Serie> getSeries(){
+		return repositoryS.findAll();
+	}
 }
